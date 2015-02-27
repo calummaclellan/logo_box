8a50fb825a8bc313eaaddd29d3421dff17228429
calummaclellan <olafsshop@aol.com>
2015-02-27 17:39:57 +0000

    comment

---
+++ logoBox/urls.py
@@ -0,0 +1,9 @@
+__author__ = '0701052m'
+
+from django.conf.urls import patterns, url
+from logoBox import views
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'),
+        url(r'^')
+        )
