1258e49ee79959d3553f75205e936e31648cb1dc
calummaclellan <olafsshop@aol.com>
2015-02-27 15:17:19 +0000

    first add

---
+++ logo_box/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for logo_box project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logo_box.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
