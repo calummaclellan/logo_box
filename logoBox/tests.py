584b96d6354e5cfffb6575a92b4a46eaff4670ac
al0014 <ringpylon@gmail.com>
2015-03-24 13:58:33 +0000

    test update

--- logoBox/tests.py
+++ logoBox/tests.py
@@ -10,6 +10,8 @@ class PostTestCase(TestCase):
     def setUp(self):
         Post.objects.create(id=1 ,content='test content 1',poster_id='old_MacTesty',likes=11,dislikes=99, category = 'testCase1')
         Post.objects.create(id=2, content='test content 2',poster_id='old_MacTestys_Brother',likes=110,dislikes=99, category = 'testCase2')
+        User.objects.create_user(username='testuser')
+        self.client.login(username='testuser')
 
     def test_get_post_by_category(self):
         #testing if we can find a post by its category tag
@@ -19,7 +21,15 @@ class PostTestCase(TestCase):
         posts = Post.objects.all()
         print get_tagged(request,'testCase2')
         print posts
-        self.assertEqual(get_tagged(request,'testCase2'),'testCase2')
+        self.assertEqual(get_tagged(request,'testCase2'),'testCase2 Content')
+
+    def test_login(self):
+        from logoBox.views import user_login
+        self.factory=RequestFactory()
+        request = self.factory.get('username')
+        c= Client.get(Client())
+        print c.get(user_login(request),{'username': 'testuser'})
+        self.assertEqual(c.get(user_login(request),{'username'}),'testuser')
 
     def test_likes(self):
         from logoBox.views import like_post
@@ -28,12 +38,6 @@ class PostTestCase(TestCase):
         request = self.factory.get('/logoBox/tag/testCase2')
         post_id= 2
         self.assertEqual(like_post(request),110)
-
-    def test_login(self):
-        from logoBox.views import user_login
-        c= Client()
-        print c.get('user_login()',{'username': 'john', 'password': 'smith'})
-        self.assertEqual(c.get(),'test')
 
 
 # Create your tests here.
