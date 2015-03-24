f8d23c5b6f5b49763d7da8011eb39292a172753e
calummaclellan <olafsshop@aol.com>
2015-03-24 13:27:06 +0000

    been adding to tests

--- logoBox/tests.py
+++ logoBox/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
 from logoBox.models import Post
-
+from django.test.client import RequestFactory
 
 #we don't really know how to write these.
 
@@ -13,9 +13,27 @@ class PostTestCase(TestCase):
 
     def test_get_post_by_category(self):
         #testing if we can find a post by its category tag
+        from logoBox.views import get_tagged
+        self.factory=RequestFactory()
+        request = self.factory.get('/logoBox/tag/testCase2')
         posts = Post.objects.all()
-        self.assertEqual()
+        print get_tagged(request,'testCase2')
+        print posts
+        self.assertEqual(get_tagged(request,'testCase2'),'testCase2')
 
+    def test_likes(self):
+        from logoBox.views import like_post
+        self.factory=RequestFactory()
+
+        request = self.factory.get('/logoBox/tag/testCase2')
+        post_id= 2
+        self.assertEqual(like_post(request),110)
+
+    def test_login(self):
+        from logoBox.views import user_login
+        c= Client()
+        print c.get('user_login()',{'username': 'john', 'password': 'smith'})
+        self.assertEqual(c,'test')
 
 
 # Create your tests here.
